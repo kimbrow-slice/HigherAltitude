f820e36d48227cd6f06dd7402bdf2a3ddf1d3cfa
Jeffery Kimbrow <71730781+KimbrowSkilledKC@users.noreply.github.com>
2020-10-13 14:23:57 -0500

    bug fix #2
    Read More js fixed

--- readmore.js
+++ readmore.js
@@ -1,15 +1,12 @@
 function myReadMore() {
-    var dots = document.getElementById("dots");
     var moreText = document.getElementById("more");
     var btnText = document.getElementById("myBtn");
 
-    if (dots.style.display === "none") {
-        dots.style.display = "inline";
+    if (moreText.style.display === "none") {
+        btnText.innerHTML = "Read less";
+        moreText.style.display = "block";
+    } else {
         btnText.innerHTML = "Read more";
         moreText.style.display = "none";
-    } else {
-        dots.style.display = "none";
-        btnText.innerHTML = "Read less";
-        moreText.style.display = "inline";
     }
 }
